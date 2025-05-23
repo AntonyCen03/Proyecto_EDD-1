2a58f51868b57f46d93219247dee31e447535d99
Antony Cen <antony.cen@correo.unimet.edu.ve>
2025-05-22 23:29:07 -0400

    se agrego las primitiva en la lista

--- src/proyecto_edd/pkg1/Lista.java
+++ src/proyecto_edd/pkg1/Lista.java
@@ -13,11 +13,61 @@ public class Lista {
     public Nodo pLast;
 
     public Lista(Nodo pFirst, Nodo pLast) {
-        this.pFirst = pFirst;
-        this.pLast = pLast;
+        this.pFirst = null;
+        this.pLast = null;
     }
     
+    public boolean EsVacia(){
+        return this.pFirst==null;
+    }
     
+    public Nodo Primero(){
+        return this.pFirst;
+    }
+    
+    public Nodo Ultimo(){
+        return this.pLast;
+    }
+    
+    public String Leer(Nodo pValor){
+        return pValor.dato;
+    }
+    
+    public Nodo Proximo(Nodo pValor){
+        if (pValor!=this.Ultimo()) {
+            return pValor.pNext;
+        }else{
+            return this.Ultimo();
+        }
+    }
+    
+    public Nodo Final(){
+        if (this.EsVacia()) {
+            return null;
+        }else{
+            Nodo pAux=this.Primero();
+            while (pAux.pNext!=this.Ultimo()){
+                pAux=this.Proximo(pAux);
+            }
+            return pAux;
+        }
+    }
+    
+    public String Recoorer(){
+        Nodo pAux;
+        String resultado="";
+        if (this.EsVacia()) {
+            return "La lista esta vacia";
+          
+        }else{
+            pAux=this.Primero();
+            while (pAux!=this.Ultimo()){
+                resultado=resultado+this.Leer(pAux)+"   ";
+                pAux=this.Primero();
+            }
+        }
+        return resultado;
+    }
     
     
 }
