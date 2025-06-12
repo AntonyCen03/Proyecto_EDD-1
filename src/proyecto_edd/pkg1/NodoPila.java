2e6c26b5b2fc046e2bd725eddc8ec76498a5eaae
Antony Cen <antony.cen@correo.unimet.edu.ve>
2025-06-12 01:14:03 -0400

    Se agrego Pila y NodoPila

---
+++ src/proyecto_edd/pkg1/NodoPila.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_edd.pkg1;
+
+/**
+ *
+ * @author Antony Cen
+ */
+public class NodoPila {
+    private Object dato;
+    private NodoPila pNext;
+
+    public NodoPila(Object dato) {
+        this.dato = dato;
+        this.pNext = null;
+    }
+
+    /**
+     * @return the dato
+     */
+    public Object getDato() {
+        return dato;
+    }
+
+    /**
+     * @param dato the dato to set
+     */
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    /**
+     * @return the pNext
+     */
+    public NodoPila getpNext() {
+        return pNext;
+    }
+
+    /**
+     * @param pNext the pNext to set
+     */
+    public void setpNext(NodoPila pNext) {
+        this.pNext = pNext;
+    }
+    
+    
+}
---
+++ src/proyecto_edd/pkg1/Pila.java
@@ -0,0 +1,115 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_edd.pkg1;
+
+/**
+ *
+ * @author anton
+ */
+public class Pila {
+    private NodoPila pIni;
+    private int Size;
+
+    public Pila() {
+        this.pIni = null;
+        this.Size = 0;
+    }
+    
+    /**
+     * @return the pIni==null
+     */
+    public boolean EsVacio(){
+        return pIni==null;
+    }
+    
+    /**
+     * @return the Size
+     */
+    public int Tamaño(){
+        return Size;
+    }
+    
+    /**
+     * @return the pIni.getDato
+     */
+    public Object LeerCabezar(){
+        return pIni.getDato();
+    }
+    
+    /**
+     * Metodo Apilar
+     */
+    public void Apilar(Object dato){
+        NodoPila nuevo=new NodoPila(dato);
+        if (EsVacio()) {
+            pIni=nuevo;
+        }else{
+            NodoPila aux=getpIni();
+            nuevo.setDato(aux);
+            pIni=nuevo;
+        }
+    }
+    
+    /**
+     * Metodo Desapilar
+     */
+    public void Desapilar(){
+        if (! EsVacio()) {
+            pIni.getpNext();
+            Size--;
+        }
+    }
+    
+    
+    /**
+     * Metodo de Imprimir
+     * @return the Print
+     */
+    public String Print(){
+        String texto;
+        texto="";
+        if (! EsVacio()) {
+            NodoPila aux=pIni;
+            for (int i = 0; i < getSize(); i++) {
+                texto=aux.getDato()+",";
+                aux=aux.getpNext();
+            }
+        }else{
+            texto="La píla esta vacio";
+        }
+        return texto;
+    } 
+
+    /**
+     * @return the pIni
+     */
+    public NodoPila getpIni() {
+        return pIni;
+    }
+
+    /**
+     * @param pIni the pIni to set
+     */
+    public void setpIni(NodoPila pIni) {
+        this.pIni = pIni;
+    }
+
+    /**
+     * @return the Size
+     */
+    public int getSize() {
+        return Size;
+    }
+
+    /**
+     * @param Size the Size to set
+     */
+    public void setSize(int Size) {
+        this.Size = Size;
+    }
+    
+    
+    
+}
